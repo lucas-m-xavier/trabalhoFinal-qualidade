99a1d332601a1cb6b7037ed53d7ab191c73c8df6
Gabriel-mDias <gabrielmagdias@gmail.com>
2021-10-02 16:18:00 -0300

    Nomes de variáveis e outros code smells

--- src/main/java/bonusfuncionario/MetodoCalculoBonusCargo.java
+++ src/main/java/bonusfuncionario/MetodoCalculoBonusCargo.java
@@ -6,18 +6,18 @@ import model.Funcionario;
 public class MetodoCalculoBonusCargo implements IMetodoCalculaBonus {
 
     //Constante
-    private static String CARGO = "Cargo: ";
+    private static String cargo = "Cargo: ";
     
     @Override
     public void calcular(Funcionario funcionario) {
         String cargoFuncionario = funcionario.getCargo();
 
         if (cargoFuncionario.toUpperCase().contains("GERENTE")) {
-            funcionario.addBonus(new Bonus(CARGO + cargoFuncionario, 100));
+            funcionario.addBonus(new Bonus(cargo + cargoFuncionario, 100));
         } else if (cargoFuncionario.toUpperCase().contains("SUPERVISOR")) {
-            funcionario.addBonus(new Bonus(CARGO + cargoFuncionario, 80));
+            funcionario.addBonus(new Bonus(cargo + cargoFuncionario, 80));
         } else if (cargoFuncionario.toUpperCase().contains("PROGRAMADOR")) {
-            funcionario.addBonus(new Bonus(CARGO + cargoFuncionario, 50));
+            funcionario.addBonus(new Bonus(cargo + cargoFuncionario, 50));
         }
     }
 }
